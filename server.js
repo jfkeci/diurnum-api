d72f8e3d8eb5687776e79082527fe377a26c277f
filip <ja.lip132@gmail.com>
2021-09-13 09:24:53 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const winston = require('winston')
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+const { createLogger } = require('winston')
+
+app.use(cors())
+
+const usersRoute = require('./routes/users')
+
+
+const PORT = process.env.PORT || 3000
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//logger
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize({ all: true })
+            )
+        }),
+        new winston.transports.File({ filename: 'error.log', level: 'error' })
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({ filename: 'exceptions.log' })
+    ]
+});
+
+//routes
+app.use('/bbetter/users', usersRoute)
+
+//connect to mongodb atlas
+mongoose
+    .connect(
+        process.env.DATABASE_URI,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    ).then(() => {
+        logger.info("Connected to mongodb atlas")
+    }).catch(error => {
+        logger.error(error.message)
+    })
+
+app.listen(PORT, () => {
+    logger.info(`Server started at port: ${PORT}`)
+})
