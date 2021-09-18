de9a7ac9cd15d4b46108d9cde9c7a4850d262d52
jfkeci <ja.lip132@gmail.com>
2021-09-18 18:08:49 +0200

    chore: template schema

---
+++ models/template.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+//Template schema
+const TemplateSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 255
+    },
+    title: {
+        type: String,
+        required: false,
+        minlength: 1,
+        maxlength: 255
+    },
+    content: {
+        type: String,
+        required: false,
+        minlength: 1,
+        maxlength: 40
+    },
+    categoryId: {
+        type: String,
+        required: false,
+        minlength: 5,
+        maxlength: 250
+    },
+    private: {
+        type: Boolean,
+        default: true
+    }
+}, { timestamps: true })
+
+
+module.exports = Template = new mongoose.model('Template', TemplateSchema) //named export
